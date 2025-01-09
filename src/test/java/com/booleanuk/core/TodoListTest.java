e0bee06c8760e17099df445c7e33c95fd460e51c
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-09 14:21:45 +0100

    First test

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,21 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 class TodoListTest {
     @Test
     public void exampleTest() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    public void tryToAddToTodoList(){
+        TodoList todoList = new TodoList();
+        String taskName = "Go do exercise";
+
+        Assertions.assertTrue(todoList.add(taskName));
     }
 }
