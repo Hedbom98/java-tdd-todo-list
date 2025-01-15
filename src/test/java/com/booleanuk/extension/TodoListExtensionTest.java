9cc78f93855466b26dc475a781f26670d9192abb
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-15 16:37:45 +0100

    Initial test for getTaskByID() in TodoListExtension completed

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -1,5 +1,8 @@
 package com.booleanuk.extension;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,10 +11,18 @@ public class TodoListExtensionTest {
         TodoItem todoItem1 = new TodoItem(1, "Clean room", true);
         add(todoItem1);
 
-        TodoItem todoItem2 = new TodoItem(1, "Buy groceries", false);
+        TodoItem todoItem2 = new TodoItem(2, "Buy groceries", false);
         add(todoItem2);
 
-        TodoItem todoItem3 = new TodoItem(1, "Write CV", false);
+        TodoItem todoItem3 = new TodoItem(3, "Write CV", false);
         add(todoItem3);
     }};
+
+    @Test
+    public void enteringNonExistingID(){
+        TodoListExtension todoList = new TodoListExtension(items);
+        Integer nonExistingID = 4;
+
+        Assertions.assertFalse(todoList.getTaskByID(nonExistingID));
+    }
 }
